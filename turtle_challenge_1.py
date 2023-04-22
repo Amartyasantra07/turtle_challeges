9599416338672e74ea114686083918a6af3e0e50
Amartya Santra <97338866+Amartyasantra07@users.noreply.github.com>
2023-04-22 17:11:09 +0530

    Add files via upload

---
+++ turtle_challenge_1.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+
+suku = Turtle()
+suku.shape("turtle")
+suku.color('red')
+suku.forward(150)
+suku.right(90)
+suku.forward(150)
+suku.right(90)
+suku.forward(150)
+suku.right(90)
+suku.forward(150)
+suku.right(90)
+screen = Screen()
+
+screen.exitonclick()
